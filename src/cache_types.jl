f63e49fb1e5362fa4e470966589253dfb9677c5f
William Chen <chenwilliamy77@gmail.com>
2020-10-21 01:04:05 -0400

    Make out-of-place functions also cache.

--- src/cache_types.jl
+++ src/cache_types.jl
@@ -16,20 +16,19 @@ end
 
 function RALF1(f::Function, x1::C1, array_type::DataType, dims::NTuple{N, Int};
                chunksize::Int = length(x1)) where {C1 <: AbstractArray{<: Number}, N}
-    cache = array_type(undef, ntuple(x -> 0, length(dims))) # Create empty array first, just to check if f is in place or not
+    cache = array_type(undef, dims)
     if applicable(f, cache, x1)
-        cache = array_type(undef, dims)
         fnew = function _f_ip(cache::LCN, x1::C1N) where {LCN <: DiffCache, C1N <: AbstractArray{<: Number}}
             f(get_tmp(cache, x1), x1)
             return get_tmp(cache, x1)
         end
-        return RALF1(fnew, dualcache(cache, Val{chunksize}))
     else
-        fnew = function _f_oop(cache::LCN, x1::C1N) where {LCN <: Nothing, C1N <: AbstractArray{<: Number}}
-            return f(x1)
+        fnew = function _f_oop(cache::LCN, x1::C1N) where {LCN <: DiffCache, C1N <: AbstractArray{<: Number}}
+            get_tmp(cache, x1) .= f(x1)
+            return get_tmp(cache, x1)
         end
-        return RALF1(fnew, nothing)
     end
+    return RALF1(fnew, dualcache(cache, Val{chunksize}))
 end
 
 function RALF1(fin::LC) where {LC <: AbstractArray{<: Number}}
@@ -49,10 +48,8 @@ end
 function RALF2(f::Function, x1::C1, x2::C2, array_type::DataType,
                dims::NTuple{N, Int}, chunksizes::NTuple{Nc, Int} =
                (length(x1) + length(x2), )) where {C1 <: AbstractArray{<: Number}, C2 <: AbstractArray{<: Number}, N, Nc}
-    cache = array_type(undef, ntuple(x -> 0, length(dims))) # Create empty array first, just to check if f is in place or not
+    cache = array_type(undef, dims)
     if applicable(f, cache, x1, x2)
-        cache = array_type(undef, dims)
-
         if length(chunksizes) == 1 # Figure out which type of DiffCache is needed
             diffcache = dualcache(cache, Val{chunksizes[1]})
             fnew      = function _f_ip1(cache::LCN, x1::C1N, x2::C2N, select::Tuple{Int, Int}) where {LCN <: DiffCache,
@@ -80,16 +77,36 @@ function RALF2(f::Function, x1::C1, x2::C2, array_type::DataType,
         else
             throw(MethodError("The length of the sixth input argument, chunksizes, must be 1, 2, or 3."))
         end
-
-        return RALF2(fnew, diffcache)
     else
-        fnew = function _f_oop(cache::LCN, x1::C1N, x2::C2N, select::Tuple{Int,Int}) where {LCN <: Nothing,
-                                                                                            C1N <: AbstractArray{<: Number},
-                                                                                            C2N <: AbstractArray{<: Number}}
-            return f(x1, x2)
+        if length(chunksizes) == 1 # Figure out which type of DiffCache is needed
+            diffcache = dualcache(cache, Val{chunksizes[1]})
+            fnew      = function _f_oop1(cache::LCN, x1::C1N, x2::C2N, select::Tuple{Int, Int}) where {LCN <: DiffCache,
+                                                                                                     C1N <: AbstractArray{<: Number},
+                                                                                                     C2N <: AbstractArray{<: Number}}
+                get_tmp(cache, x1, x2, select) .= f(x1, x2)
+                return get_tmp(cache, x1, x2, select)
+            end
+        elseif length(chunksizes) == 2
+            diffcache = twodualcache(cache, Val{chunksizes[1]}, Val{chunksizes[2]})
+            fnew      = function _f_oop2(cache::LCN, x1::C1N, x2::C2N, select::Tuple{Int, Int}) where {LCN <: TwoDiffCache,
+                                                                                                     C1N <: AbstractArray{<: Number},
+                                                                                                     C2N <: AbstractArray{<: Number}}
+                get_tmp(cache, x1, x2, select) .= f(x1, x2)
+                return get_tmp(cache, x1, x2, select)
+            end
+        elseif length(chunksizes) == 3
+            diffcache = threedualcache(cache, Val{chunksizes[1]}, Val{chunksizes[2]}, Val{chunksizes[3]})
+            fnew      = function _f_oop3(cache::LCN, x1::C1N, x2::C2N, select::Tuple{Int, Int}) where {LCN <: ThreeDiffCache,
+                                                                                                     C1N <: AbstractArray{<: Number},
+                                                                                                     C2N <: AbstractArray{<: Number}}
+                get_tmp(cache, x1, x2, select) .= f(x1, x2)
+                return get_tmp(cache, x1, x2, select)
+            end
+        else
+            throw(MethodError("The length of the sixth input argument, chunksizes, must be 1, 2, or 3."))
         end
-        return RALF2(fnew, nothing)
     end
+    return RALF2(fnew, diffcache)
 end
 
 function RALF2(fin::LC) where {LC <: AbstractArray{<: Number}}
