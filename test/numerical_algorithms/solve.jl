843bab85e8a4f89b0c82ab5184259c9867c9c19f
William Chen <chenwilliamy77@gmail.com>
2020-10-21 23:36:54 -0400

    Update some of the tolerances since initial guesses are no longer the right answer in solve. Add broken tests for homotopy in solve.jl if autodiffing, too.

--- test/numerical_algorithms/solve.jl
+++ test/numerical_algorithms/solve.jl
@@ -20,38 +20,38 @@ yguess = 1.01 .* copy(ral.y)
 # relaxation w/finite diff Jacobian
 solve!(ral, zguess, yguess; verbose = :high, autodiff = :central, ftol = 1e-8) # first w/ calculating the deterministic steady state
 @test ral.z ≈ sssout["z"]                                                      # and then proceeding to stochastic steady state
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 update!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ)
 solve!(ral; verbose = :none, autodiff = :central, ftol = 1e-8) # Now just go straight to solving stochastic steady state
-@test ral.z ≈ sssout["z"]
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.z ≈ sssout["z"] atol=1e-6
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ;
        verbose = :none, autodiff = :central, ftol = 1e-8) # Now just go straight to solving stochastic steady state
-@test ral.z ≈ sssout["z"]
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.z ≈ sssout["z"] atol=1e-6
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 # homotopy w/finite diff Jacobian
 solve!(ral, zguess, yguess;
        verbose = :high, algorithm = :homotopy, autodiff = :central, ftol = 1e-8) # first w/ calculating the deterministic steady state
 @test ral.z ≈ sssout["z"]                                                      # and then proceeding to stochastic steady state
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 update!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ)
 solve!(ral; verbose = :none, algorithm = :homotopy, autodiff = :central, ftol = 1e-8) # Now just go straight to solving stochastic steady state
 @test ral.z ≈ sssout["z"]
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ;
        verbose = :none, algorithm = :homotopy, autodiff = :central, ftol = 1e-8) # Now just go straight to solving stochastic steady state
 @test ral.z ≈ sssout["z"]
-@test ral.y ≈ sssout["y"] atol=5e-7
+@test ral.y ≈ sssout["y"] atol=1e-6
 @test ral.Ψ ≈ sssout["Psi"]
 
 
@@ -59,9 +59,10 @@ solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ;
 solve!(ral, zguess, yguess; verbose = :high, autodiff = :forward, ftol = 1e-8)
 update!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ)
 solve!(ral; verbose = :high, autodiff = :forward, ftol = 1e-8)
-solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ; verbose = :none, autodiff = :forward, ftol = 1e-8) # currently can't autodiff b/c caching problem
+solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ; verbose = :none, autodiff = :forward, ftol = 1e-8)
 
-solve!(ral, zguess, yguess; verbose = :high, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy)
+# currently can't autodiff w/homotopy b/c chunksize inference is not working
+@test_broken solve!(ral, zguess, yguess; verbose = :high, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy)
 update!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ)
-solve!(ral; verbose = :high, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy)
-solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ; verbose = :none, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy) # currently can't autodiff b/c caching problem
+@test_broken solve!(ral; verbose = :high, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy)
+@test_broken solve!(ral, 1.01 .* z, 1.01 .* y, 1.01 .* Ψ; verbose = :none, autodiff = :forward, ftol = 1e-8, algorithm = :homotopy)
