c7bfa0822e8554bb616689b6df6f39717497467a
William Chen <chenwilliamy77@gmail.com>
2020-10-25 22:13:22 -0400

    Create impulse responses function.

---
+++ src/simulation/impulse_responses.jl
@@ -0,0 +1,44 @@
+"""
+```
+impulse_responses(m, horizon, shock_ind, shock_size, z₀)
+impulse_responses(m, horizon, shock_ind, shock_size)
+```
+
+calculates impulse responses according to a risk-adjusted linearization,
+given a size for the shock, the index of the shock, and the initial state.
+The second method assumes that the initial state is the stochastic steady state.
+
+### Inputs
+- `m::RiskAdjustedLinearization`: a solved risk-adjusted linearization of a dynamic economy
+- `z₀::AbstractVector`: the initial state from which the economy begins
+- `horizon::Int`: number of periods to be simulated
+- `shock_ind::Int`: index of the shock that should be nonzero (other shocks are zero)
+- `shock_size::Number`: size of the shock (can be positive or negative)
+
+### Outputs
+- `states`: a matrix of the simulated path of states `z`, with type specified by the array type of `z₀`
+- `jumps`: a matrix of the simulated path of jump variables `y`, with type specified by the array type of `z₀`
+"""
+function impulse_responses(m::RiskAdjustedLinearization, horizon::Int, shock_ind::Int,
+                           shock_size::Number, z₀::AbstractVector)
+
+    # Create shock vector and output matrices
+    shock            = zeros(eltype(z₀), m.Nε, 1)
+    shock[shock_ind] = shock_size
+    states           = similar(z₀, m.Nz, horizon)
+    jumps            = similar(z₀, m.Ny, horizon)
+
+    # Calculate state after impact
+    states[:, 1], jumps[:, 1] = simulate(m, 1, shock, z₀)
+
+    # Simulate with no other shocks drawn
+    states[:, 2:end], jumps[:, 2:end] = simulate(m, horizon - 1, (@view states[:, 1]))
+
+    return states, jumps
+end
+
+function impulse_responses(m::RiskAdjustedLinearization, horizon::Int, shock_ind::Int, shock_size::Number)
+
+    # Calculate starting at stochastic steady state
+    return impulse_responses(m, horizon, shock_ind, shock_size, m.z)
+end
