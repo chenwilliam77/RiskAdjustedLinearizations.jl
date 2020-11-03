2b53c19b98e69cf3af7cb540e4166f77ce250c32
William Chen <chenwilliamy77@gmail.com>
2020-11-02 23:12:39 -0500

    Add extra tests for Blanchard-Kahn to cover deterministic case.

--- test/numerical_algorithms/blanchard_kahn.jl
+++ test/numerical_algorithms/blanchard_kahn.jl
@@ -10,9 +10,20 @@ y = vec(sssout["y"])
 # Create model and update it
 m = WachterDisasterRisk()
 ral = inplace_wachter_disaster_risk(m)
-RiskAdjustedLinearizations.update!(ral, z, y, Ψ)
-@info "The following message about Blanchard-Kahn conditions is expected."
+update!(ral, z, y, Ψ)
+@info "The following messages about Blanchard-Kahn conditions are expected."
 @test RiskAdjustedLinearizations.blanchard_kahn(ral)
+@test RiskAdjustedLinearizations.blanchard_kahn(ral; verbose = :low)
 @test RiskAdjustedLinearizations.blanchard_kahn(ral; verbose = :none)
+
+# Get deterministic steady state
+detout = JLD2.jldopen(joinpath(dirname(@__FILE__), "../reference/det_ss_output.jld2"), "r")
+z = vec(detout["z"])
+y = vec(detout["y"])
+
+update!(ral, z, y, zeros(length(y), length(z)))
+RiskAdjustedLinearizations.compute_Ψ(ral; zero_entropy_jacobian = true)
+@test RiskAdjustedLinearizations.blanchard_kahn(ral; deterministic = true, verbose = :low)
+@test RiskAdjustedLinearizations.blanchard_kahn(ral; deterministic = true, verbose = :high)
 
 nothing
